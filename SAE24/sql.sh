1972cd358ba6c56ce09059daa0536964e8e81119
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-06-25 01:11:55 +0200

    Delete SAE24 directory

--- SAE24/sql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-x=$1
-y=$2
-query="INSERT INTO \`position\` (\`id_pos\`, \`pos_x\`, \`pos_y\`, \`date/heure\`) VALUES (NULL, '$x', '$y', CURRENT_TIMESTAMP)"
-/opt/lampp/bin/mysql -h localhost -u max -pMaxime23@ -D sae24 -e "$query"
-
