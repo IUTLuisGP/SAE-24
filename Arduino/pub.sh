d84029e9dec8c046a9eb0282776d60a0f35e56fb
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-06-26 00:55:58 +0200

    Add files via upload

--- Arduino/pub.sh
+++ Arduino/pub.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Put in variable the given arguments #
 ampli1=$1
 ampli2=$2
 ampli3=$3
 
+# Publish the data on the mosquitto MQTT brocker with 1 second interval so that we can receive on three subscribers #
 mosquitto_pub -h localhost -p 1883 -t iut/bate/E102/01 -m "{\"ampli\":\"$ampli1\"}"
 sleep 1
 mosquitto_pub -h localhost -p 1883 -t iut/bate/E102/10 -m "{\"ampli\":\"$ampli2\"}"
