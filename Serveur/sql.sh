f7cffbae01cc3b34c42f81f6ea22695d29f24dc9
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-06-26 00:51:52 +0200

    Add files via upload

--- Serveur/sql.sh
+++ Serveur/sql.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+# Put in variable the given arguments #
 x=$1
 y=$2
+
+# Inserts the data in the database #
 query="INSERT INTO \`position\` (\`id_pos\`, \`pos_x\`, \`pos_y\`, \`date/heure\`) VALUES (NULL, '$x', '$y', CURRENT_TIMESTAMP)"
-# For Linux users :/opt/lampp/bin/mysql -h localhost -u max -pMaxime23@ -D sae24 -e "$query"
-/Applications/mamp/Library/bin/mysql -h localhost -u max -pMaxime23@ -D sae24 -e "$query"
+/opt/lampp/bin/mysql -h localhost -u max -pMaxime23@ -D sae24 -e "$query"
+
